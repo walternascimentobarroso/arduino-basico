855098228113399a6c75b88e9461cb5d14ca4aab
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2016-06-02 16:10:11 -0400

    [ADD] Codigo para o led pulsar
    Signed-off-by: Walter Nascimento <walter.nascimento.barroso@gmail.com>

---
+++ projeto7 - Lâmpada pulsante/lampada_pulsante/lampada_pulsante.ino
@@ -0,0 +1,16 @@
+// Projeto 7 – Lâmpada pulsante
+int ledPin = 11;
+float sinVal;
+int ledVal;
+void setup() {
+	 pinMode(ledPin, OUTPUT);
+}
+void loop() {
+	 for (int x=0; x<180; x++) {
+		 // converte graus para radianos e, então, obtém o valor do seno
+		 sinVal = (sin(x*(3.1416/180)));
+		 ledVal = int(sinVal*255);
+		 analogWrite(ledPin, ledVal);
+		 delay(25);
+	 }
+}
