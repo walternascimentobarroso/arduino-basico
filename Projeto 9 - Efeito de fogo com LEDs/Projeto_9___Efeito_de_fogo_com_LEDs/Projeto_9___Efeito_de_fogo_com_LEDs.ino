96e6bbe6a1429c92f74e1c5bcfa79cdd7dc7c42d
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2016-06-24 20:04:18 -0400

    [ADD] Projeto 9 do livro de arduino
    Signed-off-by: Walter Nascimento <walter.nascimento.barroso@gmail.com>

---
+++ Projeto 9 - Efeito de fogo com LEDs/Projeto_9___Efeito_de_fogo_com_LEDs/Projeto_9___Efeito_de_fogo_com_LEDs.ino
@@ -0,0 +1,15 @@
+// Projeto 9 – Efeito de fogo com LEDs
+int ledPin1 = 9;
+int ledPin2 = 10;
+int ledPin3 = 11;
+void setup() {
+	 pinMode(ledPin1, OUTPUT);
+	 pinMode(ledPin2, OUTPUT);
+	 pinMode(ledPin3, OUTPUT);
+}
+void loop() {
+	 analogWrite(ledPin1, random(120)+136);
+	 analogWrite(ledPin2, random(120)+136);
+	 analogWrite(ledPin3, random(120)+136);
+	 delay(random(100));
+}
